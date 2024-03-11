1ff5b2ba6be2a10ee43027ee059112942d7ad6ea
jwork <jkopatsis@gmail.com>
2024-03-11 11:08:31 -0700

    post analytics as promise

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,66 +1,89 @@
 <script>
-    import { onMount } from 'svelte';
-    import { page } from '$app/stores'; // Import the page store
-    import { goto } from '$app/navigation'; // Import goto for SvelteKit navigation
-  
-    let errorMessage = '';
-  
-    onMount(async () => {
-      const { slug } = $page.params; // Destructure the slug from the page params
-  
-      try {
-        const response = await fetch(`https://cs361a.wl.r.appspot.com/entry/${slug}`);
-        const data = await response.json();
-        
-        if (data.url) {
-          // HERE! Increment the python api with slug and current time. 
-          window.location.href = data.url; // Redirects to the URL
-        } else {
-          console.error('No URL found in the response');
-          errorMessage = 'No original URL is associated with this shortened URL';
-        }
-      } catch (error) {
-        console.error('Fetch error:', error);
-        errorMessage = error.message || 'An error occurred while fetching data.';
+  import { onMount } from "svelte";
+  import { page } from "$app/stores"; // Import the page store
+  import { goto } from "$app/navigation"; // Import goto for SvelteKit navigation
+
+  let errorMessage = "";
+
+  onMount(async () => {
+    const { slug } = $page.params; // Destructure the slug from the page params
+
+    try {
+      const response = await fetch(
+        `https://cs361a.wl.r.appspot.com/entry/${slug}`,
+      );
+      const data = await response.json();
+
+      if (data.url) {
+
+        const timestamp = new Date().toISOString();
+
+        const url = "http://127.0.0.1:5000/analyze_qr_code";
+
+        const data = { qr_id: slug, timestamp: timestamp };
+
+        fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log("Success:", data);
+          })
+          .catch((error) => {
+            console.error("Error:", error);
+          });
+
+        window.location.href = data.url;
+      } else {
+        console.error("No URL found in the response");
+        errorMessage = "No original URL is associated with this shortened URL";
       }
-    });
+    } catch (error) {
+      console.error("Fetch error:", error);
+      errorMessage = error.message || "An error occurred while fetching data.";
+    }
+  });
 
-    let goHome = () => {
-      goto('./')
-    }
-  </script>
-  
-  <main>
-    {#if errorMessage}
-      <h1>Error: {errorMessage}</h1>
-      <h2>But you can add one here: </h2>
-      <button on:click={goHome}>Return Home</button>
-    {/if}
-  </main>
+  let goHome = () => {
+    goto("./");
+  };
+</script>
 
-  <style>
-    h1{
-        font-size: 1.5em;
-    }
-    h2{
-        font-size: 1.25em;
-    }
-    main{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
-    button {
-		padding: 1rem;
-		background: rgba(255, 255, 255, 0.5);
-		border-radius: 2px;
-		border: none;
-	}
+<main>
+  {#if errorMessage}
+    <h1>Error: {errorMessage}</h1>
+    <h2>But you can add one here:</h2>
+    <button on:click={goHome}>Return Home</button>
+  {/if}
+</main>
 
-	button:focus,
-	button:hover {
-		background: var(--color-theme-1);
-		color: white;
-		outline: none;
-	}
-  </style>
+<style>
+  h1 {
+    font-size: 1.5em;
+  }
+  h2 {
+    font-size: 1.25em;
+  }
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  button {
+    padding: 1rem;
+    background: rgba(255, 255, 255, 0.5);
+    border-radius: 2px;
+    border: none;
+  }
+
+  button:focus,
+  button:hover {
+    background: var(--color-theme-1);
+    color: white;
+    outline: none;
+  }
+</style>
