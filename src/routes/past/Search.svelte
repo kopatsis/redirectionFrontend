62a0cc2ae6543688863c2ecf7bde42f9c9a902f6
jwork <jkopatsis@gmail.com>
2024-03-12 13:41:56 -0700

    give search bar a clear search option

--- src/routes/past/Search.svelte
+++ src/routes/past/Search.svelte
@@ -1,15 +1,17 @@
 <script>
-    import { data } from '$lib/stores/linkstore.js';
+    import { data } from "$lib/stores/linkstore.js";
     export let entryArray;
 
     let searchval = "";
     let timeout;
 
     let filterEntries = (sv) => {
-        const search = String(sv).toLowerCase()
+        const search = String(sv).toLowerCase();
         if (search !== "") {
-            entryArray = data.filter((entry) =>
-                entry.param.toLowerCase().includes(search) || entry.url.toLowerCase().includes(search),
+            entryArray = data.filter(
+                (entry) =>
+                    entry.param.toLowerCase().includes(search) ||
+                    entry.url.toLowerCase().includes(search),
             );
         } else {
             entryArray = data;
@@ -24,8 +26,18 @@
             filterEntries(searchval);
         }, 500);
     }
-
 </script>
 
 <div>Filter by original URL or new URL names:</div>
-<input type="text" placeholder="Search..." bind:value={searchval} />
+<div class="searchcontain">
+    <input type="text" placeholder="Search..." bind:value={searchval} />
+    {#if searchval !== ""}
+        <button on:click={() => (searchval = "")}>X</button>
+    {/if}
+</div>
+
+<style>
+    .searchcontain {
+        display: flex;
+    }
+</style>
