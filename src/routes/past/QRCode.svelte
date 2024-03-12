36283b8de74c20f17fb20522df513ac0b9e6de3e
jwork <jkopatsis@gmail.com>
2024-03-12 15:21:58 -0700

    qr code download button

--- src/routes/past/QRCode.svelte
+++ src/routes/past/QRCode.svelte
@@ -1,24 +1,46 @@
 <script>
-    import { onMount } from 'svelte';
-    import QRCode from 'qrcode';
-  
-    export let QRText = '';
-  
-    let qrImageURL = '';
-  
+    import { onMount } from "svelte";
+    import QRCode from "qrcode";
+
+    export let QRText = "";
+
+    let qrImageURL = "";
+
     onMount(async () => {
-      if (QRText) {
-        try {
-          qrImageURL = await QRCode.toDataURL(QRText);
-        } catch (err) {
-          console.error(err);
+        if (QRText) {
+            try {
+                qrImageURL = await QRCode.toDataURL(QRText);
+            } catch (err) {
+                console.error(err);
+            }
         }
-      }
     });
-  </script>
-  
-  {#if qrImageURL}
-    <img src={qrImageURL} alt="QR Code" />
-  {:else}
+</script>
+
+{#if qrImageURL}
+<div class="contain-qr">
+   <button class="qrbutton"
+        ><a href={qrImageURL} download="qrcode.png" class="download-button">
+            Download
+        </a>
+    </button>
+    <img src={qrImageURL} alt="QR Code" class="qrcode" /> 
+</div>
+    
+{:else}
     <div>Generating QR code...</div>
-  {/if}
+{/if}
+
+<style>
+    .contain-qr{
+        display: flex;
+        flex-direction: column;
+    }
+    .qrbutton{
+        width: fit-content;
+    }
+    /* img.qrcode {
+        height: 222px;
+        width: 222px
+    } */
+</style>
