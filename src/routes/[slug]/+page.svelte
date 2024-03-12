143207bba0d85a472a467fc8e12b345cc790744b
James Kopatsis <jam@Meatball.local>
2024-03-12 13:14:42 -0700

    fix redirect issue

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -20,18 +20,18 @@
 
         const url = "http://127.0.0.1:5000/analyze_qr_code";
 
-        const data = { qr_id: slug, timestamp: timestamp };
+        const poster = { qr_id: slug, timestamp: timestamp };
 
-        fetch(url, {
+        await fetch(url, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify(data),
+          body: JSON.stringify(poster),
         })
           .then((response) => response.json())
-          .then((data) => {
-            console.log("Success:", data);
+          .then((resp) => {
+            console.log("Success:", resp);
           })
           .catch((error) => {
             console.error("Error:", error);
