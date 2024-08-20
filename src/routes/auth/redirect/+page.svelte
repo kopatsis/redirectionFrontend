a7c36f434a1d6b3a4c250693b8ac74bd30445841
jwork <jkopatsis@gmail.com>
2024-08-20 16:39:10 -0700

    google redir

---
+++ src/routes/auth/redirect/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    import { getAuth, getRedirectResult } from "firebase/auth";
+    import { goto } from "$app/navigation";
+
+    async function handleRedirect() {
+        const auth = getAuth();
+
+        try {
+            const result = await getRedirectResult(auth);
+
+            if (result && result.user) {
+                goto("./");
+            } else {
+                goto("./login");
+            }
+        } catch (error) {
+            goto("./login");
+        }
+    }
+
+    handleRedirect();
+</script>
+
+<p>Processing sign-in...</p>
